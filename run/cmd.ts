3844de190ff840d474185c6acd63943a0ed0b8a4
Dean Xu <373216024@qq.com>
2021-03-03 18:24:50 +0800

    [run] default watch

--- run/cmd.ts
+++ run/cmd.ts
@@ -5,9 +5,7 @@ export const Command = new cli.Command()
   .description("run project")
   .allowEmpty()
   .option('-p, --port <port:number>', 'server port number', {default: 8000})
-  .option('-w, --watch [watch:boolean]', 'watch file change to reload', {default: false})
+  .option('-w, --watch [watch:boolean]', 'watch file change to reload', {default: true})
   .action(async (options: any, name: string) => {
     await run(options)
   });
-
-
