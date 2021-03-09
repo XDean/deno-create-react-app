fc722829d881c1e5f6601da36a0f2426355dbcff
dxu <dean.xu@asml.com>
2021-03-09 10:43:58 +0800

    upgrade dpendency

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-export * as cli from "https://deno.land/x/cliffy@v0.17.2/command/mod.ts";
+export * as cli from "https://deno.land/x/cliffy@v0.18.0/command/mod.ts";
 export * as oak from "https://deno.land/x/oak@v6.5.0/mod.ts";
 
 export * as fs from 'https://deno.land/std@0.88.0/fs/mod.ts';
@@ -6,4 +6,4 @@ export * as path from 'https://deno.land/std@0.88.0/path/mod.ts';
 export * as server from 'https://deno.land/std@0.88.0/http/server.ts';
 export * as datetime from 'https://deno.land/std@0.69.0/datetime/mod.ts'
 
-export { open } from "https://deno.land/x/opener/mod.ts";
+export { open } from "https://deno.land/x/opener@v1.0.1/mod.ts";
